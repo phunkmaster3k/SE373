a37db34b9759dc069dcba3ef77554264ec678eb3
Nathan Livernois <nslivernois@email.neit.edu>
2017-10-07 09:45:11 -0400

    First Commit

---
+++ public_html/week1/demo/server3/server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (request, response) {
+    
+    /* Will give you the path name with no parameters */
+    var pathName = url.parse(request.url).pathname;
+    var foo = pathName.split('/')
+
+
+   /* Send the HTTP header 
+    * HTTP Status: 200 : OK
+    * Content Type: text/html 
+    */
+   response.writeHead(200, {'Content-Type': 'text/html'});
+   
+   response.write('<!DOCTYPE html><html><body><div>Request for ' + pathName + '<p>' +foo[0]+ '</p>' + foo[1]  + '</div></body></html>');
+   response.end();
+   
+}).listen(3000);
+
+/* Console will print the message */
+console.log('Server running at http://localhost:3000/');
+
+
