d08d7006d517f6ddcc72a773af5b6d983d89996b
Nathan Livernois <nslivernois@email.neit.edu>
2017-10-12 19:13:48 -0400

    Lab2 created

--- public_html/week2/lab2/app.js
+++ public_html/week2/lab2/app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var debug = require('debug')('demo:app');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -22,11 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function (req, res, next) {
-    debug('Time:', Date.now());
-    next()
-});
 
 app.use('/', index);
 app.use('/users', users);
