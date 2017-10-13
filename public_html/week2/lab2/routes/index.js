9f8103dce7cfe85376c05fa5b459952ca7023b21
Nathan Livernois <nslivernois@email.neit.edu>
2017-10-13 11:44:07 -0400

    Lab code done, just needs bootstrap

--- public_html/week2/lab2/routes/index.js
+++ public_html/week2/lab2/routes/index.js
@@ -10,4 +10,12 @@ router.get('/about', function(req, res, next) {
     res.render('about', { title: 'About Page' });
 });
 
+router.get('/form', function(req, res, next) {
+    res.render('form', { title: 'Form Page' });
+});
+
+router.post('/form', function(req, res, next) {
+    res.render('form', { title: 'Form Page', email: req.body.email, name: req.body.name, comment: req.body.comment });
+});
+
 module.exports = router;
