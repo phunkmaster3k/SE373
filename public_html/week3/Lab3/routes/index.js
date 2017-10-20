8b54d7cd37075abfc827160da0bb6dcf54fad65c
Nathan Livernois <nslivernois@email.neit.edu>
2017-10-20 14:15:51 -0400

    lab 3 complete needs bs

--- public_html/week3/Lab3/routes/index.js
+++ public_html/week3/Lab3/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var debug = require('debug')('Lab3:form');
+var menuItems = [2,4,5,10,20];
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/form', function(req, res, next) {
+
+
+    res.render('form', { title: 'Form Page', items: menuItems });
+});
+
+router.post('/form', function(req, res, next) {
+    res.render('form', { items: menuItems, pickVal: req.body.picker });
+});
 module.exports = router;
